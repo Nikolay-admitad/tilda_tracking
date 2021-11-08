f3aee5ba2672f33fab21ec97811e324c7f5d8f69
Nikolay-admitad <73425205+Nikolay-admitad@users.noreply.github.com>
2021-11-08 12:20:19 +0300

    Order web hooks handler

---
+++ order_web_hooks_handler.php
@@ -0,0 +1,54 @@
+<?php
+
+$posback_request = array();
+$posback_request['postback_key'] = '8ADeD3A1998cA3841F5f2899DcC12eF3';
+$posback_request['campaign_code'] = 'b519ef0db6';
+$posback_request['action_code'] = '3';
+$posback_request['tariff_code'] = '1';
+$posback_request['pb'] = '1';
+
+$posback_request['order_id'] = $_POST['payment']['orderid'];
+$posback_request['price'] = $_POST['payment']['amount'];
+
+$tilda_cookies = $_POST["COOKIES"];
+
+$cookies = explode("; ", $tilda_cookies);
+foreach ($cookies as &$value) {
+if (strpos($value, 'deduplication_cookie') !== false) // получаем последний платный источник трафика
+    {
+        $deduplication_cookie = explode("=", $value);
+        $deduplication_cookie_value = $deduplication_cookie[1];
+    }
+
+if (strpos($value, 'tagtag_aid') !== false) // получаем admitad UID
+    {
+        $admitad_uid_cookie = explode("=", $value);
+        $posback_request['uid'] = $admitad_uid_cookie[1];
+    }
+if (strpos($value, 'TILDAUTM') !== false) // последние метки tilda
+    {
+        $tildaUTM_cookie = explode("=", $value);
+        $posback_request['tilda_utms'] = json_encode($tildaUTM_cookie);
+    }
+}
+
+$parts = array();
+foreach ($posback_request as $key => $value) {
+	$parts[] = $key . '=' . urlencode($value);
+}
+
+$url = 'https://ad.admitad.com/r?' . implode('&', $parts);
+
+header('Access-Control-Allow-Origin: *');
+
+$headers = "From: from@webhookservesite.ru";
+
+ @mail('n.kocherga@admitad.com', 'Tilda TEST', json_encode($posback_request), $headers);
+/* замените электронную почту на вашу действующую почту,  на которую вам будет приходить письмо  */
+
+echo"ok";
+
+if ($deduplication_cookie_value == 'admitad' || $$deduplication_cookie_value == 'adm'){
+	file_get_contents($url);
+} 
+?>
